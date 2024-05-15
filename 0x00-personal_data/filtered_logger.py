445638ab5710531005884ebb21392e6f5d1946b8
aezzahir <aero.glowsky@gmail.com>
2024-05-15 21:08:09 +0100

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
 """
-0. Regex-ing
+filter_datum function module
 """
+from typing import List
 import re
+import logging
 
 
-def filter_datum(fields, redaction, message, separator):
-    """filter_datum returns the log message obfuscated:"""
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Returns the log message obfuscated
+    """
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
-    return re.sub(pattern, lambda m: m.group(0).split('=')[0]
-                  + f'={redaction}', message)
+    return re.sub(pattern, lambda m:
+                  m.group(0).split('=')[0] + f'={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
