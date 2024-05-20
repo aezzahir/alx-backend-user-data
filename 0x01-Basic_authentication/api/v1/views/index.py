0dc0d43c6e2715e49b53d37219d0c869e57c1c47
aezzahir <aero.glowsky@gmail.com>
2024-05-20 22:50:32 +0100

    1. Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,6 +27,9 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """
+    raise a 401 error
+    """
     abort(401)
