a7609846055d6acfc0cdf0b367d195222b656033
aezzahir <aero.glowsky@gmail.com>
2024-05-21 21:31:48 +0100

    11. Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -38,3 +38,42 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        parts = decoded_base64_authorization_header.split(':', 1)
+        return parts[0], parts[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        9. Basic - User credentials
+        """
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+        users = User.search({'email': user_email})
+        if len(users) == 0:
+            return None
+        if not users[0].is_valid_password(user_pwd):
+            return None
+        return users[0]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        10. Basic - User object
+        """
+        header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(header)
+        decode_header = self.decode_base64_authorization_header(base64_header)
+        user_email, user_pwd = self.extract_user_credentials(decode_header)
+        return self.user_object_from_credentials(user_email=user_email,
+                                                 user_pwd=user_pwd)
