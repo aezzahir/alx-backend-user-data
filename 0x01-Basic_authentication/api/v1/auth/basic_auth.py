df17ab22a34e77256451332e66decc406ffe6b81
aezzahir <aero.glowsky@gmail.com>
2024-05-21 21:33:11 +0100

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,9 @@
 """
 6. Basic auth
 """
-from flask import request
-from typing import List, TypeVar, Optional
 from api.v1.auth.auth import Auth
+from typing import Tuple, TypeVar
+from models.user import User
 import base64
 
 
