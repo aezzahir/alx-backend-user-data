9b2691f965e7d7f231a1499c6dc9e8280188a77f
aezzahir <aero.glowsky@gmail.com>
2024-05-23 21:43:34 +0100

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,7 +32,7 @@ def before_request_handler():
         return
 
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
@@ -41,6 +41,10 @@ def before_request_handler():
 
     if auth.current_user(request) is None:
         abort(403)
+
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        return None, abort(401)
+
     request.current_user = auth.current_user(request)
 
 
