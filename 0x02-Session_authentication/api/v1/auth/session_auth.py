dea1902e418847863d7a455a46c681c1061d5716
aezzahir <aero.glowsky@gmail.com>
2024-05-23 21:56:22 +0100

    6. Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,3 +32,15 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return (None)
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """6. Use Session ID for identifying a User"""
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id in None:
+            return None
+        user = User.get(user_id)
+        return User
