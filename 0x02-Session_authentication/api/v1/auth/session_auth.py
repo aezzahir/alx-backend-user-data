ece65676fa64b2dafa10b2661425efd932b43c49
aezzahir <aero.glowsky@gmail.com>
2024-05-23 21:23:35 +0100

    2. Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,8 +7,20 @@ from typing import List, TypeVar, Optional, Tuple, TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
 import base64
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """SessionAuth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session"""
+
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return (None)
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
