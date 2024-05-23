2683894174a59a1c64153ed650bdfc2bc8fdbf45
aezzahir <aero.glowsky@gmail.com>
2024-05-23 21:36:11 +0100

    3. User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -33,3 +34,12 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """that returns None - request will be the Flask request object"""
         return None
+
+    def session_cookie(self, request=None):
+        """Session cookie"""
+        if request is None:
+            return None
+        session_name = os.getenv("SESSION_NAME")
+        if session_name is None:
+            return None
+        return request.cookies.get(session_name)
