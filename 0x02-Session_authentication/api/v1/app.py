bb415866adb429f87009888d36307f6dc4dacb03
aezzahir <aero.glowsky@gmail.com>
2024-05-23 21:13:18 +0100

    1. Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,6 +43,7 @@ def before_request_handler():
         abort(403)
     request.current_user = auth.current_user(request)
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
